59aef68c27fc6937a9fd7f032bd7e7ba497593c6
Mohammed Kabir Dastgir <mkd@Mohammeds-MacBook-Pro.local>
2025-12-07 23:04:47 +0000

    2025 Day 7 WIP

--- 2025/Day7/index.js
+++ 2025/Day7/index.js
@@ -64,8 +64,57 @@ const testData = `.......S.......
 ...............`;
 
 let run = "actual";
-// run = "test";
+run = "test";
 
 const input = run === "actual" ? data : testData;
 
-console.log("\n", countTachyonSplits(input));
+// console.log("\n", countTachyonSplits(input));
+
+function countTachyonTimelineSplits(input) {
+  const lines = input.split("\n");
+  let splitCount = 0;
+
+  const startingPoint = lines[0].indexOf("S");
+  const tachyonBeamLocation = startingPoint;
+
+  function traverseTree(lines, tachyonBeamLocation, direction) {
+    let count = 0;
+
+    for (let i = 1; i < lines.length; i++) {
+      const line = lines[i];
+
+      if (line[tachyonBeamLocation] === ".") continue;
+
+      console.log(
+        line.slice(0, tachyonBeamLocation - 1) +
+          "|^" +
+          line.slice(tachyonBeamLocation + 1),
+        tachyonBeamLocation
+      );
+
+      count += traverseTree(lines.slice(i + 1), tachyonBeamLocation - 1, -1);
+
+      console.log(
+        line.slice(0, tachyonBeamLocation) +
+          "^|" +
+          line.slice(tachyonBeamLocation + 2),
+        tachyonBeamLocation
+      );
+      count += traverseTree(lines.slice(i + 1), tachyonBeamLocation + 1, 1);
+      break;
+    }
+
+    console.log("\n");
+
+    if (lines.length === 1) return 1 + count;
+    return count;
+  }
+
+  const output = traverseTree(lines.slice(1), tachyonBeamLocation, -1);
+
+  console.log(output);
+
+  return splitCount;
+}
+
+console.log("\n", countTachyonTimelineSplits(input));
